692198a7ff550e46cc19c7267e0383dae613131b
Shimada666 <649940882@qq.com>
2021-10-23 14:31:52 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: 'Simple Blog',
+    description: '一些简单的东西',
+    // base: '/vuepress/',
+    // configureWebpack: {
+    //     resolve: {
+    //         alias: {
+    //             '@alias': './assets/'
+    //         }
+    //     }
+    // },
+    dest: './dist',
+    themeConfig: {
+        lastUpdated: 'Last Updated',
+        collapsable: true,
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '博客', link: '/blog/' },
+            { text: 'Github', link: 'https://github.com/Shimada666' }
+        ],
+        sidebar: {
+            '/blog/': [
+                ''
+            ]
+        },
+        sidebarDepth: 3
+    }
+};
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /diana.jpg
+actionText: 开始阅读 →
+actionLink: /blog/
+footer: MIT Licensed | Copyright © 2021 Shimada666
+---
