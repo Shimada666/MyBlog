bb8977012b7527ad6573d128c48a51a4c4cd6270
Shimada666 <649940882@qq.com>
2022-03-12 23:50:41 +0800

    feat: 添加评论系统

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import DefaultTheme from 'vitepress/theme'
+const { Layout } = DefaultTheme
+
+</script>
+
+<template>
+  <Layout>
+    <template #page-bottom-ads>
+      <component
+          is="script"
+          src="https://beaudar.lipk.org/client.js"
+          repo="Shimada666/MyBlog"
+          label="Comment"
+          issue-term="pathname"
+          theme="github-light"
+          crossorigin="anonymous"
+          async></component>
+    </template>
+  </Layout>
+</template>
