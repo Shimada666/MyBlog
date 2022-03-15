cf4d5220ecf08fec0bcfa782f5035fd44f718ad2
Shimada666 <649940882@qq.com>
2022-03-15 19:05:36 +0800

    feat: 兼容初次访问时也能使用评论系统

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,14 +1,37 @@
 <script lang="ts" setup>
 import DefaultTheme from 'vitepress/theme'
+import { watch } from 'vue'
+import { useData } from "vitepress"
 
 const { Layout } = DefaultTheme
+
+function refreshBeaudar() {
+  const beaudar = document.getElementById('beaudar');
+  if (!beaudar) {
+    return
+  }
+  beaudar.innerHTML = ''
+  var script = document.createElement('script');
+  script.src = 'https://beaudar.lipk.org/client.js';
+  script.setAttribute('repo', 'Shimada666/MyBlog');
+  script.setAttribute('issue-term', 'pathname');
+  script.setAttribute('crossorigin', 'anonymous');
+  script.setAttribute('label', 'Comment')
+  script.setAttribute('theme', 'github-light');
+  script.async = true;
+  beaudar.appendChild(script);
+}
+
+const { title } = useData()
+
+watch(title, () => refreshBeaudar())
 
 </script>
 
 <template>
   <Layout>
     <template #page-bottom-ads>
-      <div style="margin-top: 16px;">
+      <div style="margin-top: 16px;" id="beaudar">
         <component
           is="script"
           src="https://beaudar.lipk.org/client.js"
